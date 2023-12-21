7b7dc78a06aa815e571529c02601a6e8d9022df6
Huzaifa Sabahuddin <huzaifasabah@gmail.com>
2023-12-21 14:28:57 +0000

    Updated

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from jinja2 import TemplateNotFound
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-load_dotenv('databaseinfo.env')
+# load_dotenv('databaseinfo.env')
 # app.config['MYSQL_HOST'] = os.environ.get('DB_HOST')
 # app.config['MYSQL_USER'] = os.environ.get('DB_USER')
 # app.config['MYSQL_PORT'] = os.environ.get('DB_PORT')
